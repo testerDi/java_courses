1e35d79c6d582c0dfa7448ddcbb7bf856f733ea4
Diana <diana.rozumniak@gmail.com>
2017-02-17 19:32:49 +0200

    ex2

---
+++ ex2/com/Greeting.java
@@ -0,0 +1,6 @@
+package com;
+
+
+public class Greeting{
+public String text = "world!";
+}
---
+++ ex2/test/TestGreeting.java
@@ -0,0 +1,12 @@
+package test;
+
+import com.Greeting;
+public class TestGreeting{
+	public static void main(String[]  args){ 
+		Greeting g = new Greeting();
+		System.out.println("Hello," + g.text);
+
+		
+
+}
+}
