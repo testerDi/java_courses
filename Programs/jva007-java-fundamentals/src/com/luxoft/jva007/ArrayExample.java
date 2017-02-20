1eed49b895a5b51a2f8a6b14475af304e28aa08f
Diana <diana.rozumniak@gmail.com>
2017-02-20 19:41:35 +0200

    lesson3

---
+++ Programs/jva007-java-fundamentals/src/com/luxoft/jva007/ArrayExample.java
@@ -0,0 +1,10 @@
+package com.luxoft.jva007;
+
+/**
+ * Created by ptcvas3 on 2/20/2017.
+ */
+public class ArrayExample {
+    public static void main(String[] args){
+        int [][] ints = new int [3][5];
+    }
+}
---
+++ Programs/jva007-java-fundamentals/src/com/luxoft/jva007/PrimitiveExample.java
@@ -0,0 +1,23 @@
+package com.luxoft.jva007;
+
+/**
+ * Created by ptcvas3 on 2/20/2017.
+ */
+public class PrimitiveExample {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 10;
+       int  c = foo(a, b);
+        System.out.println("a = " + a + ", b = " + b+ ", c = " + c);
+        int nums[] = new int[];
+    }
+
+    static int foo(int x, int y){
+        x = 15;
+        int r = x + y;
+        return r;
+    }
+    static void boo(int[] ints){
+        ints[1] = 10;
+    }
+}
